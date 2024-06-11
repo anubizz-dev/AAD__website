75ef9e7be4af742d9a73b493c88bc5c9d67224aa
anubizz-dev <danielngoho@icloud.com>
2024-06-11 22:43:41 +0800

    Service cards UI fixed
    Service cards UI fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,26 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    ["./src/**/*.{html,js}"],
   ],
   theme: {
-    extend: {},
+    extend: {}, 
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [require('daisyui'),],
 
