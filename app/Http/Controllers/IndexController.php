91816c745e2a6f1ad780ecfaad9f2a5303c6896c
Ariez Flores (Aquilino) <aries.flores@gmail.com>
2024-06-12 18:22:26 +0800

    Added sending contact us messages
    Added sending contact us messages

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,11 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\InquiryEmail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class IndexController extends Controller
 {
     public function index(){
         return view('index');
+    }
+
+    public function sendMail(Request $request){
+        $request->validate([
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => 'email|required',
+            'message' => 'required',
+        ]);
+
+        $email = 'inquiry@aadsoftware-services.com';
+
+        Mail::to($email)->send(
+            new InquiryEmail(
+                $request->input('firstname'),
+                $request->input('lastname'),
+                $request->input('email'),
+                $request->input('message')
+            ));
+
+        return redirect()->back()->with('success', 'Inquiry sent succesfully.');
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -230,7 +230,13 @@
             </p>
            
         </div>
-        <form class="md:col-span-8 p-10">
+        <form class="md:col-span-8 p-10" method="POST" action="{{route('sendemail')}}">
+          @if (session('success'))
+              <div class="text-center text-red-500 my-10">
+                  {{session('success')}}
+              </div>
+          @endif
+          @csrf
             <div class="flex flex-wrap -mx-3 mb-6">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -239,8 +245,15 @@
                     </label>
                     <input
                         class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-                        id="grid-first-name" type="text" placeholder="Jane">
-                 
+                        id="grid-first-name" 
+                        type="text" 
+                        placeholder="Jane"
+                        id="firstname"
+                        name="firstname"
+                    >
+                  @error('firstname')
+                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                  @enderror
                 </div>
                 <div class="w-full md:w-1/2 px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -249,7 +262,15 @@
                     </label>
                     <input
                         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-last-name" type="text" placeholder="Phoenix">
+                        id="grid-last-name" 
+                        type="text" 
+                        placeholder="Phoenix"
+                        id="lastname"
+                        name="lastname"
+                    >
+                    @error('lastname')
+                      <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                    @enderror
                 </div>
             </div>
             <div class="flex flex-wrap -mx-3 mb-6">
@@ -260,7 +281,15 @@
                     </label>
                     <input
                         class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                        id="grid-email" type="email" placeholder="********@*****.**">
+                        id="grid-email" 
+                        type="email" 
+                        placeholder="********@*****.**"
+                        id="email"
+                        name="email"
+                    >
+                    @error('email')
+                      <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                    @enderror
                 </div>
             </div>
 
@@ -271,9 +300,15 @@
                         Your Message
                     </label>
                     <textarea rows="10"
-                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"></textarea>
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="message"
+                        name="message"
+                        ></textarea>
+                    @error('message')
+                      <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                    @enderror
                 </div>
-                <div class="flex justify-between w-full px-3">
+                <div class="flex justify-between w-full px-3 mt-10">
                     <button
                         class="shadow bg-[#5B61DC] hover:bg-violet-700 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
                         type="submit">
